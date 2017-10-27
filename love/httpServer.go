cac0e2755c25966b55109dc1ac06c906c61291f5
王渊 <799086345@qq.com>
2017-10-27 18:14:05 +0800

    add some files

---
+++ love/httpServer.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	// "fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
+)
+
+func main() {
+
+	log.SetFlags(log.Lshortfile | log.Flags())
+
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", StaticServer)
+	serveMux.HandleFunc("/bye", SayBye)
+	serveMux.HandleFunc("/ha", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hahahha,funny"))
+	})
+
+	server := http.Server{
+		Addr:        ":8080",
+		Handler:     serveMux,
+		ReadTimeout: 5 * time.Second,
+	}
+
+	log.Println("http server start,port is ", 8080)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetFlags(log.Ltime)
+
+}
+
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	if ok, _ := regexp.MatchString("/static/", r.URL.String()); ok {
+		StaticServer(w, r)
+		return
+	} else {
+		const a = "{\"name\":\"wangyuan\",\"age\":23}"
+		io.WriteString(w, a)
+	}
+}
+
+func SayBye(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Byebye")
+}
+
+func StaticServer(w http.ResponseWriter, r *http.Request) {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.StripPrefix("/",
+		http.FileServer(http.Dir(wd))).ServeHTTP(w, r)
+
+}
