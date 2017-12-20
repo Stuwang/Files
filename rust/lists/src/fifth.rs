1f23040dbe15ae09fcdbbc08d8ade524a1ab86ec
王渊 <799086345@qq.com>
2017-12-20 22:55:43 +0800

    update

---
+++ rust/lists/src/fifth.rs
@@ -0,0 +1,22 @@
+use std::ptr;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: *mut Node<T>, // DANGER DANGER
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: ptr::null_mut(),
+        }
+    }
+}
--- rust/lists/src/main.rs
+++ rust/lists/src/main.rs
@@ -2,6 +2,7 @@ mod first;
 mod second;
 mod third;
 mod fourth;
+mod fifth;
 
 fn main() {
     first::try_main();
