6e3585ba1b7b402d099ff2a41fd90d3ed06ba876
wangyuan <799086345@qq.com>
2018-01-01 22:33:56 +0800

    重命名文件，修改文件

--- python-parse/Parse1/Parse.py
+++ python-parse/Parse1/Parse.py
@@ -1,7 +1,39 @@
 import ply.yacc as yacc
+import logging;
 
 # Get the token map from the lexer.  This is required.
-from calclex import tokens
+from calc_token import *;
+import calc_token;
+
+# this is a function to debug
+def PrintTokens(data,lex ):
+    PrintTokens = True
+    lex.input(data)
+    if PrintTokens == True :
+        lex.input(data)
+        errorOccr = False;
+        while True:
+            tok = lex.token()
+            if not tok:
+                errorOccr = False;
+                print("error occr")
+                break
+            print(tok)
+        if errorOccr:
+            print("token parse ok!");
+    print("============================")
+
+# dictionary of names
+names = { }
+
+def p_statement_assign(p):
+    'statement : NAME "=" expression'
+    names[p[1]] = p[3]
+    print(p[3])
+
+def p_statement_expr(p):
+    'statement : expression'
+    print(p[1])
 
 def p_expression_plus(p):
     'expression : expression PLUS term'
@@ -35,6 +67,14 @@ def p_factor_expr(p):
     'factor : LPAREN expression RPAREN'
     p[0] = p[2]
 
+def p_expression_name(p):
+    "factor : NAME"
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = 0
+
 # Error rule for syntax errors
 def p_error(p):
     print("Syntax error in input!")
@@ -42,14 +82,23 @@ def p_error(p):
 # Build the parser
 parser = yacc.yacc()
 
-result = parser.parse("1+(2+3)")
-print(result)
+log = logging.getLogger()
 
-# while True:
-#    try:
-#        s = raw_input('calc > ')
-#    except EOFError:
-#        break
-#    if not s: continue
-#    result = parser.parse(s)
-#    print(result)
+
+
+data = '''
+1 + 1
+'''
+
+PrintTokens(data,calc_token.lexer);
+
+# result = parser.parse(data,debug=log)
+# print(result)
+
+while 1:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+    if not s: continue
+    yacc.parse(s)
---
+++ python-parse/calc.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+
+
+# -----------------------------------------------------------------------------
+# calc.py
+#
+# A simple calculator with variables.   This is from O'Reilly's
+# "Lex and Yacc", p. 63.
+# -----------------------------------------------------------------------------
+
+# 简单 四则运算 的计算器
+
+import sys;
+
+if sys.version_info[0] >= 3:
+    raw_input = input
+
+tokens = (
+    'NAME','NUMBER',
+    )
+
+# Tokens
+t_NAME   = r'[a-zA-Z][a-zA-Z0-9_]*'
+
+literals = ['=','+','-','*','/', '(',')','^']
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lex.lex()
+
+# Parsing rules
+
+precedence = (
+    ('left','+','-'),
+    ('left','*','/'),
+    ('left','^'),
+    ('right','UMINUS'),
+    )
+
+# dictionary of names
+names = { }
+
+def p_statement_assign(p):
+    'statement : NAME "=" expression'
+    names[p[1]] = p[3]
+
+def p_statement_expr(p):
+    'statement : expression'
+    print(p[1])
+
+def p_expression_binop(p):
+    '''expression : expression '+' expression
+                  | expression '-' expression
+                  | expression '*' expression
+                  | expression '^' expression
+                  | expression '/' expression'''
+    if p[2] == '+'  : p[0] = p[1] + p[3]
+    elif p[2] == '-': p[0] = p[1] - p[3]
+    elif p[2] == '*': p[0] = p[1] * p[3]
+    elif p[2] == '/': p[0] = p[1] / p[3]
+    elif p[2] == '^': p[0] = p[1] **p[3]
+
+def p_expression_uminus(p):
+    "expression : '-' expression %prec UMINUS"
+    p[0] = -p[2]
+
+def p_expression_group(p):
+    "expression : '(' expression ')'"
+    p[0] = p[2]
+
+def p_expression_number(p):
+    "expression : NUMBER"
+    p[0] = p[1]
+
+def p_expression_name(p):
+    "expression : NAME"
+    try:
+        p[0] = names[p[1]]
+    except LookupError:
+        print("Undefined name '%s'" % p[1])
+        p[0] = 0
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+import ply.yacc as yacc
+yacc.yacc()
+
+# this is a function to debug
+def PrintTokens(data,lex = lex.lexer):
+    PrintTokens = True
+    lex.input(data)
+    if PrintTokens == True :
+        lex.input(data)
+        while True:
+            tok = lex.token()
+            if not tok:
+                print("error occr")
+                break
+            print(tok)
+        print("token parse ok!");
+    print("============================")
+
+PrintTokens("1+(2+3)");
+
+while 1:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+    if not s: continue
+    yacc.parse(s)
